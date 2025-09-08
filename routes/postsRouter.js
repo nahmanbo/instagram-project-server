a85e59b5aafe639d55cdfbc2d659e130a5dcea55
nahmanbo <nhmanbo2003@gmail.com>
2025-09-08 12:30:06 +0300

    Finish creating a router for posts

---
+++ routes/postsRouter.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import {
+  getAllPosts,
+  getPostById,
+  addPost,
+  updatePost,
+  deletePost
+} from "../controllers/postsContrillers.js";
+
+
+
+const router = Router();
+
+// Get all posts
+router.get("/", getAllPosts);
+
+// Get posts by ID 
+router.get("/id/:id", getPostById);
+
+// Add new posts 
+router.post("/", addPost);
+
+// Update a posts by ID 
+router.put("/:id", updatePost);
+
+// Delete a posts by ID 
+router.delete("/:id", deletePost);
+
+export default router;

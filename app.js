1af226c069a2800714fd22025a5f8440d0ed7ac8
nahmanbo <nhmanbo2003@gmail.com>
2025-09-09 15:35:58 +0300

    add usersRouter in app and cleanup code

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import path from 'path';
 
-import postRouter from "./routes/postsRouter.js"
+import postsRouter from "./routes/postsRouter.js"
+import usersRouter from "./routes/usersRouter.js"
+
 import { logger } from "./utils/loggerHelper.js";
 
 const PORT = 1212;
 const __dirname = path.resolve();
 const app = express();
-
 
 // Middleware
 app.use(express.json());
@@ -17,7 +18,8 @@ app.use(logger);
 app.use(express.static(__dirname + '/assets/images'));
 
 // Routes
-app.use("/api/posts", postRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/users", usersRouter);
 
 // Start Server
 app.listen(PORT, () => {

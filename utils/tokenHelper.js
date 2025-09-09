7ca40390975b4c66a46426ea3d4de3b86f614097
nahmanbo <nhmanbo2003@gmail.com>
2025-09-09 17:19:42 +0300

    add token Helper

---
+++ utils/tokenHelper.js
@@ -0,0 +1,8 @@
+  // Helper to generate JWT
+  export default function generateToken(user) {
+    return jwt.sign(
+      {name: user.name},
+      process.env.JWT_SECRET || "dev-secret",
+      { expiresIn: "12h" }
+    );
+  }

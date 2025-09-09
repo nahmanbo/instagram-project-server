253240b69d4e2792db4a362f4038ec62174d950e
nahmanbo <nhmanbo2003@gmail.com>
2025-09-09 12:05:27 +0300

    Finished adding a feature for returning static files

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 import express from "express";
 import cors from "cors";
+import path from 'path';
 
 import postRouter from "./routes/postsRouter.js"
 import { logger } from "./utils/loggerHelper.js";
 
 const PORT = 1212;
+const __dirname = path.resolve();
 const app = express();
+
 
 // Middleware
 app.use(express.json());
 app.use(cors()); 
 app.use(logger);
+app.use(express.static(__dirname + '/assets/images'));
 
 // Routes
 app.use("/api/posts", postRouter);
